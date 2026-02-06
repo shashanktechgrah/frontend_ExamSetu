177f88ebdd045fcdee154b8895f2c4a5863ccb1e
shashanktechgrah <shashanktechgrah@gmail.com>
2026-02-06 17:51:08 +0530

    Update InstructionsPage.tsx

--- src/pages/InstructionsPage.tsx
+++ src/pages/InstructionsPage.tsx
@@ -1,4 +1,4 @@
-import api from "../config/api";
+import api from "../config/api.js";
 import { useEffect, useState } from 'react'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 
@@ -115,6 +115,7 @@ function InstructionsPage() {
 }
 
 export default InstructionsPage
+
 
 
 
